40c2f7c7773eb17cbb59b037a0c532ccc1a90a4c
Soumyo Deepto <reachsoumyo@gmail.com>
2026-02-25 22:57:33 +0530

    Blank deployment

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello from blank Node app Soumyo 🚀");
+  res.end("Hello from blank Node app Soumyo 🚀 ");
 };
